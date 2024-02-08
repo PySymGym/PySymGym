1d3c599248ddb5de497b49d2a2094ae6f11bbe84
Anya497 <chi.vinny0702@gmail.com>
2024-02-08 14:03:17 +0300

    Big refactor of dataset and code at all. Add saving each step in a particular file. Split running, training and validation by different files. Create dataloader. Delete support of models learned with genetic algorithms.

--- AIAgent/learning/play_game.py
+++ AIAgent/learning/play_game.py
@@ -16,37 +16,35 @@ from func_timeout import FunctionTimedOut, func_set_timeout
 from learning.timer.resources_manager import manage_map_inference_times_array
 from learning.timer.stats import compute_statistics
 from learning.timer.utils import get_map_inference_times
-from ml.data_loader_compact import ServerDataloaderHeteroVector
+from ml.dataset import convert_input_to_tensor
 from ml.fileop import save_model
 from ml.model_wrappers.protocols import Predictor
+from ml.training.dataset import TrainingDataset
 
 TimeDuration: TypeAlias = float
 
 
 def play_map(
-    with_connector: Connector, with_predictor: Predictor, with_dataset
+    with_connector: Connector, with_predictor: Predictor, with_dataset: TrainingDataset
 ) -> tuple[GameResult, TimeDuration]:
     steps_count = 0
     game_state = None
     actual_coverage = None
-    steps = with_connector.steps
+    steps = with_connector.map.StepsToPlay
 
     start_time = perf_counter()
 
     map_steps = []
 
     def add_single_step(input, output):
-        hetero_input, _ = ServerDataloaderHeteroVector.convert_input_to_tensor(input)
+        hetero_input, _ = convert_input_to_tensor(input)
         hetero_input["y_true"] = output
-        hetero_input.to(GeneralConfig.DEVICE)
         map_steps.append(hetero_input)
 
     try:
-        for _ in range(steps):
+        for _ in range(with_connector.map.StepsToPlay):
             game_state = with_connector.recv_state_or_throw_gameover()
-            predicted_state_id, nn_output = with_predictor.predict(
-                game_state, with_connector.map.MapName
-            )
+            predicted_state_id, nn_output = with_predictor.predict(game_state)
 
             add_single_step(game_state, nn_output)
 
@@ -102,6 +100,7 @@ def play_map(
             model_result.errors_count,
         )
         with_dataset.update(with_connector.map.MapName, map_result, map_steps)
+    del map_steps
     return model_result, end_time - start_time
 
 
@@ -136,7 +135,7 @@ def play_game(
     with_predictor: Predictor,
     max_steps: int,
     maps: list[GameMap],
-    with_dataset=None,
+    with_dataset: TrainingDataset,
 ):
     list_of_map2result: list[Map2Result] = []
     for game_map in maps:
@@ -173,5 +172,5 @@ def play_game(
                 / f"{with_predictor.name()}.pth",
             )
         list_of_map2result.append(Map2Result(game_map, game_result))
-        print(game_map.MapName)
-    return (list_of_map2result, with_dataset.maps_data)
+
+    return list_of_map2result
--- AIAgent/ml/training/wrapper.py
+++ AIAgent/ml/training/wrapper.py
@@ -1,16 +1,13 @@
 import copy
-import logging
-
 import torch
 from common.game import GameState
-from ml.common_model.utils import back_prop
 from ml.data_loader_compact import ServerDataloaderHeteroVector
 from ml.model_wrappers.protocols import Predictor
 
 from ml.predict import predict_state_with_dict
 
 
-class CommonModelWrapper(Predictor):
+class TrainingModelWrapper(Predictor):
     def __init__(
         self,
         model: torch.nn.Module,
@@ -20,7 +17,7 @@ class CommonModelWrapper(Predictor):
         self._name = "1"
 
     def name(self):
-        return "Common model"
+        return "Training model"
 
     def make_copy(self, model_name: str):
         self.model_copy = copy.deepcopy(self._model)
@@ -29,7 +26,7 @@ class CommonModelWrapper(Predictor):
     def model(self):
         return self._model
 
-    def predict(self, input: GameState, map_name):
+    def predict(self, input: GameState):
         hetero_input, state_map = ServerDataloaderHeteroVector.convert_input_to_tensor(
             input
         )
@@ -37,31 +34,6 @@ class CommonModelWrapper(Predictor):
 
         next_step_id, nn_output = predict_state_with_dict(
             self._model, hetero_input, state_map
-        )
-
-        del hetero_input
-        return next_step_id, nn_output
-
-
-class BestModelsWrapper(Predictor):
-    def __init__(
-        self,
-        best_models: dict,
-    ) -> None:
-        self.best_models = best_models
-
-    def name(self):
-        return "Best model"
-
-    def model(self):
-        return None
-
-    def predict(self, input: GameState, map_name):
-        hetero_input, state_map = ServerDataloaderHeteroVector.convert_input_to_tensor(
-            input
-        )
-        next_step_id, nn_output = predict_state_with_dict(
-            self.best_models[map_name][0], hetero_input, state_map
         )
 
         del hetero_input
