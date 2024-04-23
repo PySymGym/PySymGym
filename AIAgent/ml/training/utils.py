4b4084391e84115b09970c582c49b163cc01c5a7
Anya497 <chi.vinny0702@gmail.com>
2024-04-23 10:49:11 +0300

    Add dataset filter function.

--- AIAgent/ml/training/utils.py
+++ AIAgent/ml/training/utils.py
@@ -1,9 +1,13 @@
+import json
 import os
+import shutil
 import typing as t
 from pathlib import Path
 import numpy as np
 import torch
 from collections import defaultdict
+
+from common.game import GameMap
 
 
 def euclidean_dist(y_pred, y_true):
@@ -44,7 +48,7 @@ def append_to_file(file: Path, s: str):
         file.write(s)
 
 
-def find_unfinished_maps(log_file_path: Path):
+def find_unfinished_maps(log_file_path: Path) -> None:
     server_log = open(log_file_path)
     ports = defaultdict(list)
     for line in reversed(list(server_log)):
@@ -58,3 +62,16 @@ def find_unfinished_maps(log_file_path: Path):
             except ValueError:
                 print(map_name)
     server_log.close()
+
+
+def sync_dataset_with_description(dataset_path: Path, description_path: Path) -> None:
+    with open(description_path, "r") as maps_json:
+        maps_in_description = list(
+            map(
+                lambda game_map: game_map.MapName,
+                GameMap.schema().load(json.loads(maps_json.read()), many=True),
+            )
+        )
+        for saved_map in os.listdir(dataset_path):
+            if saved_map not in maps_in_description:
+                shutil.rmtree(os.path.join(dataset_path, saved_map))
