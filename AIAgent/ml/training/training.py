7b33287f8c164e0dee678c9e0c1af59f0f8e5a70
Max Nigmatulin <mvnigma@gmail.com>
2024-04-30 20:40:50 +0300

    feat: Add ONNX tests, remove commented code

--- AIAgent/ml/training/training.py
+++ AIAgent/ml/training/training.py
@@ -23,22 +23,6 @@ def train(
         batch.to(GeneralConfig.DEVICE)
         optimizer.zero_grad()
         out = infer(model, batch)
-        # out = model(
-        #     game_x=batch["game_vertex"].x,
-        #     state_x=batch["state_vertex"].x,
-        #     edge_index_v_v=batch["game_vertex", "to", "game_vertex"].edge_index,
-        #     edge_type_v_v=batch["game_vertex", "to", "game_vertex"].edge_type,
-        #     edge_index_history_v_s=batch[
-        #         "game_vertex", "history", "state_vertex"
-        #     ].edge_index,
-        #     edge_attr_history_v_s=batch[
-        #         "game_vertex", "history", "state_vertex"
-        #     ].edge_attr,
-        #     edge_index_in_v_s=batch["game_vertex", "in", "state_vertex"].edge_index,
-        #     edge_index_s_s=batch[
-        #         "state_vertex", "parent_of", "state_vertex"
-        #     ].edge_index,
-        # )
         loss = criterion(out, batch.y_true)
         if loss != 0:
             loss.backward()
--- AIAgent/ml/training/validation.py
+++ AIAgent/ml/training/validation.py
@@ -107,22 +107,6 @@ def validate_loss(
     ):
         batch.to(GeneralConfig.DEVICE)
         out = infer(model, batch)
-        # out = model(
-        #     game_x=batch["game_vertex"].x,
-        #     state_x=batch["state_vertex"].x,
-        #     edge_index_v_v=batch["game_vertex", "to", "game_vertex"].edge_index,
-        #     edge_type_v_v=batch["game_vertex", "to", "game_vertex"].edge_type,
-        #     edge_index_history_v_s=batch[
-        #         "game_vertex", "history", "state_vertex"
-        #     ].edge_index,
-        #     edge_attr_history_v_s=batch[
-        #         "game_vertex", "history", "state_vertex"
-        #     ].edge_attr,
-        #     edge_index_in_v_s=batch["game_vertex", "in", "state_vertex"].edge_index,
-        #     edge_index_s_s=batch[
-        #         "state_vertex", "parent_of", "state_vertex"
-        #     ].edge_index,
-        # )
         loss: torch.Tensor = criterion(out, batch.y_true)
         epoch_loss.append(loss.item())
     result = np.average(epoch_loss)
