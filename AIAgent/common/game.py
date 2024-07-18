fd88337b58b839d81d6991156ae9cea3b9abf90d
Anya497 <chi.vinny0702@gmail.com>
2024-07-18 13:50:48 +0300

    Fix runtime warning.

--- AIAgent/common/game.py
+++ AIAgent/common/game.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, field
+from typing import Optional
 from dataclasses_json import config, dataclass_json
 from connection.broker_conn.classes import SVMInfo as SVMInfoT
 
@@ -74,7 +75,7 @@ class GameMap:
     NameOfObjectToCover: str
     DefaultSearcher: str
     MapName: str
-    SVMInfo: SVMInfoT = field(default=None, metadata=config(exclude=lambda x: True))
+    SVMInfo: Optional[SVMInfoT] = field(default=None, metadata=config(exclude=lambda x: True))
 
 
 @dataclass_json
