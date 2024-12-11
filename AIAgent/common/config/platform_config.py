84a16629a31c289cc3e444304acbfe6a944a8733
Parzival-05 <parzivalwasd@gmail.com>
2024-12-11 19:37:23 +0300

    Remove SVMInfoViaServer

--- AIAgent/common/config/platform_config.py
+++ AIAgent/common/config/platform_config.py
@@ -1,7 +1,9 @@
 from common.config.dataset_config import DatasetConfig
 from common.typealias import PlatformName
-from common.validation_coverage.svm_info import SVMInfoViaServer
-from pydantic import Field
+from common.validation_coverage.svm_info import SVMInfo
+from pydantic import (
+    Field,
+)
 from pydantic.dataclasses import dataclass as pydantic_dataclass
 
 
@@ -9,4 +11,4 @@ from pydantic.dataclasses import dataclass as pydantic_dataclass
 class Platform:
     name: PlatformName
     dataset_configs: list[DatasetConfig] = Field(alias="DatasetConfigs")
-    svms_info: list[SVMInfoViaServer] = Field(alias="SVMSInfo")
+    svms_info: list[SVMInfo] = Field(alias="SVMSInfo")
