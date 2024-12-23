4f5d7bd4467cc4aebb34b0a816283db990b62dd4
Parzival-05 <parzivalwasd@gmail.com>
2024-12-24 02:30:12 +0300

    Specify type of `set_timeout_if_needed`

--- AIAgent/ml/validation/coverage/game_managers/utils.py
+++ AIAgent/ml/validation/coverage/game_managers/utils.py
@@ -1,10 +1,12 @@
-from typing import Any, Callable
+from typing import Callable, TypeVar
 
 from config import FeatureConfig
 from func_timeout import func_set_timeout  # type: ignore
 
+T = TypeVar("T")
 
-def set_timeout_if_needed(func: Callable[..., Any]) -> Callable[..., Any]:
+
+def set_timeout_if_needed(func: Callable[..., T]) -> Callable[..., T]:
     return (
         func_set_timeout(FeatureConfig.SAVE_IF_FAIL_OR_TIMEOUT.timeout_sec)(func)
         if FeatureConfig.SAVE_IF_FAIL_OR_TIMEOUT.enabled
