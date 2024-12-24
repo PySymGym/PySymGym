90391ca9fd600f9c2855d598a93a234927b76ffe
Parzival-05 <parzivalwasd@gmail.com>
2024-12-24 07:36:52 +0300

    Add preparation step

--- AIAgent/ml/validation/coverage/game_managers/base_game_manager.py
+++ AIAgent/ml/validation/coverage/game_managers/base_game_manager.py
@@ -8,10 +8,26 @@ from torch_geometric.data.hetero_data import HeteroData
 class BaseGameManager(ABC):
     def __init__(self):
         self._game_states: dict[str, list[HeteroData]] = {}
+        self.is_ready_to_play = False
 
-    @abstractmethod
     def play_game_map(self, game_map2svm: GameMap2SVM) -> Map2Result:
         """Executes GameMap on symbolic engine"""
+        if not self.is_ready_to_play:
+            self.prepare()
+            self.is_ready_to_play = True
+        return self._play_game_map(game_map2svm)
+
+    @abstractmethod
+    def _play_game_map(self, game_map2svm: GameMap2SVM) -> Map2Result:
+        """Executes GameMap on symbolic engine
+
+        This method is called by 'play_game_map' after the assurance that preparation step is not skipped.
+        """
+        ...
+
+    @abstractmethod
+    def prepare(self):
+        """Prepare before starting validation"""
         ...
 
     def get_game_steps(self, game_map: GameMap) -> list[HeteroData]:
