ed331e9b9ed870b027ab57669e99a4f0c0541966
Anya497 <chi.vinny0702@gmail.com>
2025-04-10 09:46:03 +0300

    Replace the previous model with one that supports path condition. Add num_pc_layers to tests configurations.

--- AIAgent/tests/test_onnx.py
+++ AIAgent/tests/test_onnx.py
@@ -7,7 +7,7 @@ import pytest
 import torch
 import yaml
 from common.game import GameState
-from ml.models.RGCNEdgeTypeTAG3VerticesDoubleHistory2Parametrized.model import (
+from ml.models.NorthernPenguin.model import (
     StateModelEncoder as RealStateModelEncoder,
 )
 from onyx import entrypoint
