0758663e58d73fabdb9214110a8cbfcaf4b32ec1
Parzival-05 <parzivalwasd@gmail.com>
2025-09-15 18:11:41 +0300

    debug

--- AIAgent/ml/dataset.py
+++ AIAgent/ml/dataset.py
@@ -739,5 +739,6 @@ def remove_extra_attrs(step: HeteroData):
 
 def get_hetero_data(game_state: GameState, nn_output: list[list[float]]) -> HeteroData:
     hetero_input, _ = convert_input_to_tensor(game_state)
+    logging.debug(nn_output)
     hetero_input["y_true"] = torch.Tensor([x[0] for x in nn_output])
     return hetero_input
