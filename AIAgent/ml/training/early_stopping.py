1d52d73a4372bfc579fba3d65088307e4025401c
Anya Chistyakova <chi.vinny0702@gmail.com>
2026-01-20 13:06:53 +0300

    Consider optimization direction in early stopping condition. (#428)

--- AIAgent/ml/training/early_stopping.py
+++ AIAgent/ml/training/early_stopping.py
@@ -1,15 +1,33 @@
+from common.config.optuna_config import OptimizationDirection
+
+
 class EarlyStopping:
-    def __init__(self, state_len: int = 5, tolerance: float = 0.01) -> None:
+    def __init__(
+        self,
+        state_len: int = 5,
+        tolerance: float = 0.01,
+        direction: OptimizationDirection = OptimizationDirection.MINIMIZE,
+    ) -> None:
         self._state_len = state_len
         self._state = list()
         self._tolerance = tolerance
+        match direction:
+            case OptimizationDirection.MINIMIZE:
+                self._get_difference = (
+                    lambda state_value: (sum(self._state) / self._state_len)
+                    - state_value
+                )
+            case OptimizationDirection.MAXIMIZE:
+                self._get_difference = lambda state_value: state_value - (
+                    sum(self._state) / self._state_len
+                )
 
     def is_continue(self, state_value) -> bool:
         if len(self._state) < self._state_len:
             self._state.append(state_value)
             return True
 
-        if (sum(self._state) / self._state_len) - state_value < self._tolerance:
+        if self._get_difference(state_value) < self._tolerance:
             return False
         else:
             self._state.pop(0)
