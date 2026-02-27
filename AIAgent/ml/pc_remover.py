9c0401e5ce0ceeb44e3f7e9772b1a176191b1970
Gitubrr <kt.spbu@gmail.com>
2026-02-27 20:23:49 +0300

    made normal names

--- AIAgent/ml/pc_remover.py
+++ AIAgent/ml/pc_remover.py
@@ -46,62 +46,66 @@ def remove_path_condition_root(heterodata: HeteroData) -> HeteroData:
         if vector[-1] != 1.0:
             nodes_path_condition.append(new_vector)
 
-    i = 0
-    for _pc_v in pathcondvertex_to_pathcondvertex[0]:
+    for indexes_of_roots_in_states, _ in enumerate(pathcondvertex_to_pathcondvertex[0]):
         if (
-            pathcondvertex_to_pathcondvertex[0][i]
+            pathcondvertex_to_pathcondvertex[0][indexes_of_roots_in_states]
             not in statevertex_to_pathcondvertex[1]
-            and pathcondvertex_to_pathcondvertex[1][i]
+            and pathcondvertex_to_pathcondvertex[1][indexes_of_roots_in_states]
             not in statevertex_to_pathcondvertex[1]
         ):
-            k1 = 0
+            shift1 = 0
             while (
-                k1 < len(statevertex_to_pathcondvertex[1])
-                and pathcondvertex_to_pathcondvertex[0][i]
-                > statevertex_to_pathcondvertex[1][k1]
+                shift1 < len(statevertex_to_pathcondvertex[1])
+                and pathcondvertex_to_pathcondvertex[0][indexes_of_roots_in_states]
+                > statevertex_to_pathcondvertex[1][shift1]
             ):
-                k1 += 1
+                shift1 += 1
 
-            k2 = 0
+            shift2 = 0
             while (
-                k2 < len(statevertex_to_pathcondvertex[1])
-                and pathcondvertex_to_pathcondvertex[1][i]
-                > statevertex_to_pathcondvertex[1][k2]
+                shift2 < len(statevertex_to_pathcondvertex[1])
+                and pathcondvertex_to_pathcondvertex[1][indexes_of_roots_in_states]
+                > statevertex_to_pathcondvertex[1][shift2]
             ):
-                k2 += 1
+                shift2 += 1
 
             edge_index_pc_pc.extend(
                 [
                     [
-                        pathcondvertex_to_pathcondvertex[0][i] - k1,
-                        pathcondvertex_to_pathcondvertex[1][i] - k2,
+                        pathcondvertex_to_pathcondvertex[0][indexes_of_roots_in_states]
+                        - shift1,
+                        pathcondvertex_to_pathcondvertex[1][indexes_of_roots_in_states]
+                        - shift2,
                     ],
                 ]
             )
-        i += 1
 
-    for _pc_v in statevertex_to_pathcondvertex[1]:
-        ind = np.where(pathcondvertex_to_pathcondvertex[0] == _pc_v)[0]
-        i = np.where(statevertex_to_pathcondvertex[1] == _pc_v)[0][0]
+    for _root_in_states in statevertex_to_pathcondvertex[1]:
+        indexes_of_roots_in_pathcond_to_pathcond = np.where(
+            pathcondvertex_to_pathcondvertex[0] == _root_in_states
+        )[0]
+        indexes_of_roots_in_states = np.where(
+            statevertex_to_pathcondvertex[1] == _root_in_states
+        )[0][0]
 
-        for _pc_v in ind:
-            k = 0
+        for index in indexes_of_roots_in_pathcond_to_pathcond:
+            shift = 0
             while (
-                pathcondvertex_to_pathcondvertex[0][_pc_v + 1]
-                > statevertex_to_pathcondvertex[1][k]
+                pathcondvertex_to_pathcondvertex[0][index + 1]
+                > statevertex_to_pathcondvertex[1][shift]
             ):
-                k += 1
+                shift += 1
 
             edge_index_pc_state.append(
                 [
-                    pathcondvertex_to_pathcondvertex[0][_pc_v + 1] - k,
-                    statevertex_to_pathcondvertex[0][i],
+                    pathcondvertex_to_pathcondvertex[0][index + 1] - shift,
+                    statevertex_to_pathcondvertex[0][indexes_of_roots_in_states],
                 ]
             )
             edge_index_state_pc.append(
                 [
-                    statevertex_to_pathcondvertex[0][i],
-                    pathcondvertex_to_pathcondvertex[0][_pc_v + 1] - k,
+                    statevertex_to_pathcondvertex[0][indexes_of_roots_in_states],
+                    pathcondvertex_to_pathcondvertex[0][index + 1] - shift,
                 ]
             )
 
