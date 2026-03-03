b4bb6d9e6c7bb708e9f3ab1d4992a53752fbf905
Ъ. <kt.spbu@gmail.com>
2026-03-03 08:49:45 +0300

    Removing pc-root from the states in python (#411)
    * removing pc-root from the states
    
    * made egjes between states and pc python
    
    * NUM_PC_FEATURES = 49 -> NUM_PC_FEATURES = 48
    
    * pcId -> pc_id
    
    * idk where is mistake
    
    * Intermediate results for tests and PS remover
    
    * added utils + tests
    
    * for removing p_root from dataset
    
    * without my tests
    
    * uncomented
    
    * formatted files + deleted unnecessary files
    
    * pc_v -> _pc_v (for linter)
    
    * made test gamestates without pc_root for onnx tests
    
    * for ruff
    
    * Update submodule reference to feature branch commit
    
    * Update VSharp submodule to .NET 8
    
    * changed json with pc_roots to json without pc_roots (for validation)
    
    * ruff formatted
    
    * moved test data for pc_remover
    
    * deleted files
    
    * made normal names
    
    * for tests
    
    * removed np.array
    
    * new fixes :)
    
    * added strict = False to zip
    
    * a lot of fixes
    
    * for ruff
    
    * changed names
    
    * little fix

---
+++ AIAgent/ml/pc_remover.py
@@ -0,0 +1,131 @@
+import numpy as np
+import torch
+from torch_geometric.data import HeteroData
+from ml.inference import TORCH
+
+
+def remove_path_condition_root(heterodata: HeteroData) -> HeteroData:
+    """
+    Removes the PathConditionRoot node from Heterodata and creates edges
+    between states and pathCondition instead.
+
+    Parameters
+    ----------
+    hetero_data : HeteroData
+        Heterodata to process.
+
+    Returns
+    -------
+    HeteroData
+        New HeteroData without PathConditionRoot.
+    """
+    new_heterodata = heterodata.clone()
+
+    path_condition_vertex = (
+        new_heterodata[TORCH.path_condition_vertex].x.detach().cpu().numpy()
+    )
+    pathcondvertex_to_pathcondvertex = (
+        new_heterodata[TORCH.pathcondvertex_to_pathcondvertex]
+        .edge_index.detach()
+        .cpu()
+        .numpy()
+    )
+    statevertex_to_pathcondvertex = (
+        new_heterodata[TORCH.statevertex_to_pathcondvertex]
+        .edge_index.detach()
+        .cpu()
+        .numpy()
+    )
+
+    edge_index_pc_pc, edge_index_pc_state, edge_index_state_pc = [], [], []
+
+    path_condition_vertex = path_condition_vertex[path_condition_vertex[:, -1] != 1.0][
+        :, :-1
+    ]
+
+    def id_corrector(el):
+        shift = 0
+        while (
+            shift < len(statevertex_to_pathcondvertex[1])
+            and el > statevertex_to_pathcondvertex[1][shift]
+        ):
+            shift += 1
+        return shift
+
+    statevertex_to_pathcondvertex_sorted = np.array(
+        sorted(statevertex_to_pathcondvertex.T, key=lambda x: x[1])
+    ).T
+
+    for pc_vertex_from, pc_vertex_to in zip(
+        *pathcondvertex_to_pathcondvertex, strict=False
+    ):
+        if (
+            pc_vertex_from not in statevertex_to_pathcondvertex_sorted[1]
+            and pc_vertex_to not in statevertex_to_pathcondvertex_sorted[1]
+        ):
+            shift1 = id_corrector(pc_vertex_from)
+
+            shift2 = id_corrector(pc_vertex_to)
+
+            edge_index_pc_pc.append(
+                [
+                    pc_vertex_from - shift1,
+                    pc_vertex_to - shift2,
+                ]
+            )
+
+    for root_in_states in statevertex_to_pathcondvertex_sorted[1]:
+        indexes_of_each_root_in_pathcond_to_pathcond = np.where(
+            pathcondvertex_to_pathcondvertex[0] == root_in_states
+        )[0]
+        index_of_root_in_states = np.where(
+            statevertex_to_pathcondvertex_sorted[1] == root_in_states
+        )[0][0]
+
+        for index in indexes_of_each_root_in_pathcond_to_pathcond:
+            child_id = pathcondvertex_to_pathcondvertex[1][index]
+            state_id = statevertex_to_pathcondvertex_sorted[0][index_of_root_in_states]
+
+            shift = id_corrector(child_id)
+
+            edge_index_pc_state.append(
+                [
+                    child_id - shift,
+                    state_id,
+                ]
+            )
+
+            edge_index_state_pc.append(
+                [
+                    state_id,
+                    child_id - shift,
+                ]
+            )
+
+    edge_index_pc_state_sorted_back = np.array(
+        sorted(edge_index_pc_state, key=lambda x: x[1])
+    )
+    edge_index_state_pc_sorted_back = np.array(
+        sorted(edge_index_state_pc, key=lambda x: x[0])
+    )
+
+    def null_if_empty(tensor):
+        return tensor if tensor.numel() != 0 else torch.empty((2, 0), dtype=torch.int64)
+
+    new_heterodata[TORCH.path_condition_vertex].x = torch.tensor(
+        path_condition_vertex, dtype=torch.float
+    )
+
+    new_heterodata[TORCH.pathcondvertex_to_pathcondvertex].edge_index = null_if_empty(
+        torch.tensor(edge_index_pc_pc, dtype=torch.long).t().contiguous()
+    )
+
+    new_heterodata[TORCH.pathcondvertex_to_statevertex].edge_index = null_if_empty(
+        torch.tensor(edge_index_pc_state_sorted_back, dtype=torch.long).t().contiguous()
+    )
+
+    new_heterodata[TORCH.statevertex_to_pathcondvertex].edge_index = null_if_empty(
+        torch.tensor(edge_index_state_pc_sorted_back, dtype=torch.long).t().contiguous()
+    )
+
+    return new_heterodata
---
+++ AIAgent/tests/test_pc_remover.py
@@ -0,0 +1,99 @@
+import numpy as np
+import os
+import pytest
+import torch
+from ml.inference import TORCH
+from ml.pc_remover import remove_path_condition_root
+
+
+@pytest.fixture
+def test_data():
+    return [
+        (
+            remove_path_condition_root(
+                torch.load(
+                    os.path.join(
+                        "tests",
+                        "resources",
+                        "heterodata_for_pc_remover",
+                        f"heterodata_with_root{i}.pt",
+                    ),
+                    weights_only=False,
+                )
+            ),
+            torch.load(
+                os.path.join(
+                    "tests",
+                    "resources",
+                    "heterodata_for_pc_remover",
+                    f"heterodata_expected{i}.pt",
+                ),
+                weights_only=False,
+            ),
+        )
+        for i in range(1, 5)
+    ]
+
+
+def test_compare_all(test_data):
+    for result, expected in test_data:
+        assert torch.equal(result[TORCH.game_vertex].x, expected[TORCH.game_vertex].x)
+        assert torch.equal(result[TORCH.state_vertex].x, expected[TORCH.state_vertex].x)
+        assert torch.equal(
+            result[TORCH.path_condition_vertex].x,
+            expected[TORCH.path_condition_vertex].x,
+        )
+        assert torch.equal(
+            result[TORCH.gamevertex_to_gamevertex].edge_index,
+            expected[TORCH.gamevertex_to_gamevertex].edge_index,
+        )
+        assert torch.equal(
+            result[TORCH.gamevertex_to_gamevertex].edge_type,
+            expected[TORCH.gamevertex_to_gamevertex].edge_type,
+        )
+        assert torch.equal(
+            result[TORCH.statevertex_in_gamevertex].edge_index,
+            expected[TORCH.statevertex_in_gamevertex].edge_index,
+        )
+        assert torch.equal(
+            result[TORCH.gamevertex_in_statevertex].edge_index,
+            expected[TORCH.gamevertex_in_statevertex].edge_index,
+        )
+        assert torch.equal(
+            result[TORCH.statevertex_history_gamevertex].edge_index,
+            expected[TORCH.statevertex_history_gamevertex].edge_index,
+        )
+        assert torch.equal(
+            result[TORCH.statevertex_history_gamevertex].edge_attr,
+            expected[TORCH.statevertex_history_gamevertex].edge_attr,
+        )
+        assert torch.equal(
+            result[TORCH.gamevertex_history_statevertex].edge_index,
+            expected[TORCH.gamevertex_history_statevertex].edge_index,
+        )
+        assert torch.equal(
+            result[TORCH.gamevertex_history_statevertex].edge_attr,
+            expected[TORCH.gamevertex_history_statevertex].edge_attr,
+        )
+        assert torch.equal(
+            result[TORCH.statevertex_parentof_statevertex].edge_index,
+            expected[TORCH.statevertex_parentof_statevertex].edge_index,
+        )
+        assert torch.equal(
+            result[TORCH.pathcondvertex_to_pathcondvertex].edge_index,
+            expected[TORCH.pathcondvertex_to_pathcondvertex].edge_index,
+        )
+        assert torch.equal(
+            result[TORCH.pathcondvertex_to_statevertex].edge_index,
+            expected[TORCH.pathcondvertex_to_statevertex].edge_index,
+        )
+        assert torch.equal(
+            result[TORCH.statevertex_to_pathcondvertex].edge_index,
+            expected[TORCH.statevertex_to_pathcondvertex].edge_index,
+        )
+
+
+def test_non_all_features_are_zeros(test_data):
+    for result, _ in test_data:
+        for vector in result[TORCH.game_vertex].x.detach().cpu().numpy():
+            assert not np.all(vector == 0)
