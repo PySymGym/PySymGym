b47185ee69e0adf1735710209730afb38fef4a58
Gitubrr <kt.spbu@gmail.com>
2026-03-06 21:19:55 +0300

    new pc remover

--- AIAgent/ml/pc_remover.py
+++ AIAgent/ml/pc_remover.py
@@ -37,83 +37,77 @@ def remove_path_condition_root(heterodata: HeteroData) -> HeteroData:
         .numpy()
     )
 
+    map_of_id = []
+    pc_x = []
     edge_index_pc_pc, edge_index_pc_state, edge_index_state_pc = [], [], []
 
-    path_condition_vertex = path_condition_vertex[path_condition_vertex[:, -1] != 1.0][
-        :, :-1
-    ]
+    roots_num = 0
+    for i, vector in enumerate(path_condition_vertex):
+        if vector[-1] != 1.0:
+            pc_x.append(vector[:-1])
+            map_of_id.append([i, i - roots_num])
+        else:
+            map_of_id.append([i, -1])
+            roots_num += 1
 
-    def id_corrector(el):
-        shift = 0
-        while (
-            shift < len(statevertex_to_pathcondvertex[1])
-            and el > statevertex_to_pathcondvertex[1][shift]
-        ):
-            shift += 1
-        return shift
-
-    statevertex_to_pathcondvertex_sorted = np.array(
-        sorted(statevertex_to_pathcondvertex.T, key=lambda x: x[1])
-    ).T
-
-    for pc_vertex_from, pc_vertex_to in zip(
-        *pathcondvertex_to_pathcondvertex, strict=False
-    ):
-        if (
-            pc_vertex_from not in statevertex_to_pathcondvertex_sorted[1]
-            and pc_vertex_to not in statevertex_to_pathcondvertex_sorted[1]
-        ):
-            shift1 = id_corrector(pc_vertex_from)
-
-            shift2 = id_corrector(pc_vertex_to)
-
-            edge_index_pc_pc.append(
-                [
-                    pc_vertex_from - shift1,
-                    pc_vertex_to - shift2,
-                ]
-            )
-
-    for root_in_states in statevertex_to_pathcondvertex_sorted[1]:
+    for root_in_states in statevertex_to_pathcondvertex[1]:
         indexes_of_each_root_in_pathcond_to_pathcond = np.where(
             pathcondvertex_to_pathcondvertex[0] == root_in_states
         )[0]
         index_of_root_in_states = np.where(
-            statevertex_to_pathcondvertex_sorted[1] == root_in_states
+            statevertex_to_pathcondvertex[1] == root_in_states
         )[0][0]
 
         for index in indexes_of_each_root_in_pathcond_to_pathcond:
             child_id = pathcondvertex_to_pathcondvertex[1][index]
-            state_id = statevertex_to_pathcondvertex_sorted[0][index_of_root_in_states]
-
-            shift = id_corrector(child_id)
+            state_id = statevertex_to_pathcondvertex[0][index_of_root_in_states]
 
             edge_index_pc_state.append(
                 [
-                    child_id - shift,
+                    map_of_id[child_id][1],
                     state_id,
                 ]
             )
 
-            edge_index_state_pc.append(
-                [
-                    state_id,
-                    child_id - shift,
-                ]
+            edge_index_state_pc.append([state_id, map_of_id[child_id][1]])
+
+    for pc_vertex_from, pc_vertex_to in zip(
+        *pathcondvertex_to_pathcondvertex, strict=False
+    ):
+        if map_of_id[pc_vertex_from][1] != -1 and map_of_id[pc_vertex_to][1] != -1:
+            edge_index_pc_pc.append(
+                [map_of_id[pc_vertex_from][1], map_of_id[pc_vertex_to][1]]
             )
 
-    edge_index_pc_state_sorted_back = np.array(
-        sorted(edge_index_pc_state, key=lambda x: x[1])
-    )
-    edge_index_state_pc_sorted_back = np.array(
-        sorted(edge_index_state_pc, key=lambda x: x[0])
-    )
+    print("path_condition_vertex old:")
+    print("num of features: ", len(path_condition_vertex[1]))
+    print("num of vertices: ", len(path_condition_vertex))
+
+    print("path_condition_vertex new:")
+    print("num of features: ", len(pc_x[1]))
+    print("num of vertices: ", len(pc_x))
+
+    print("pathcondvertex_to_pathcondvertex old:")
+    print("max el: ", max(pathcondvertex_to_pathcondvertex[1]))
+    print("min el: ", min(pathcondvertex_to_pathcondvertex[1]))
+
+    print("pathcondvertex_to_pathcondvertex new:")
+    print("max el: ", max(edge_index_pc_pc[1]))
+    print("min el: ", min(edge_index_pc_pc[1]))
+
+    print("edge_index_state_pc old:")
+    print("max el: ", max(statevertex_to_pathcondvertex[1]))
+    print("min el: ", min(statevertex_to_pathcondvertex[1]))
+
+    print("edge_index_state_pc new:")
+    print("max el: ", max(edge_index_state_pc[1]))
+    print("min el: ", min(edge_index_state_pc[1]))
 
     def null_if_empty(tensor):
         return tensor if tensor.numel() != 0 else torch.empty((2, 0), dtype=torch.int64)
 
     new_heterodata[TORCH.path_condition_vertex].x = torch.tensor(
-        path_condition_vertex, dtype=torch.float
+        pc_x, dtype=torch.float
     )
 
     new_heterodata[TORCH.pathcondvertex_to_pathcondvertex].edge_index = null_if_empty(
@@ -121,11 +115,11 @@ def remove_path_condition_root(heterodata: HeteroData) -> HeteroData:
     )
 
     new_heterodata[TORCH.pathcondvertex_to_statevertex].edge_index = null_if_empty(
-        torch.tensor(edge_index_pc_state_sorted_back, dtype=torch.long).t().contiguous()
+        torch.tensor(edge_index_pc_state, dtype=torch.long).t().contiguous()
     )
 
     new_heterodata[TORCH.statevertex_to_pathcondvertex].edge_index = null_if_empty(
-        torch.tensor(edge_index_state_pc_sorted_back, dtype=torch.long).t().contiguous()
+        torch.tensor(edge_index_state_pc, dtype=torch.long).t().contiguous()
     )
 
     return new_heterodata
